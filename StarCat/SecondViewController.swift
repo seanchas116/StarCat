fdc2b2174729e53108de0d9fe0a4f7d894662522
Ryohei Ikegami <iofg2100@gmail.com>
2015-12-19 10:46:25 +0900

    Initial Commit

---
+++ StarCat/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  StarCat
+//
+//  Created by 池上涼平 on 2015/12/19.
+//  Copyright © 2015年 seanchas116. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
