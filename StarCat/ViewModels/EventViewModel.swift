b02506da7dc1d941f81f5cfa45b8f2c224cd2dbd
Ryohei Ikegami <iofg2100@gmail.com>
2015-12-27 22:53:35 +0900

    Add view models

---
+++ StarCat/ViewModels/EventViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  EventViewModel.swift
+//  StarCat
+//
+//  Created by 池上涼平 on 2015/12/27.
+//  Copyright © 2015年 seanchas116. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class EventViewModel {
+    
+}
---
+++ StarCat/ViewModels/NewsTabViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  NewsTabViewModel.swift
+//  StarCat
+//
+//  Created by 池上涼平 on 2015/12/27.
+//  Copyright © 2015年 seanchas116. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class NewsTabViewModel {
+    let events = Variable<[EventViewModel]>([])
+}
