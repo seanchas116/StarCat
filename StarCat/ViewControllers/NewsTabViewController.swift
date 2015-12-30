10614f33c5e7bf6714cdd6c41c4d81b8db500328
Ryohei Ikegami <iofg2100@gmail.com>
2015-12-30 18:02:46 +0900

    Add NewsTabViewController

---
+++ StarCat/ViewControllers/NewsTabViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NewsTabViewController.swift
+//  StarCat
+//
+//  Created by Ryohei Ikegami on 2015/12/30.
+//  Copyright © 2015年 seanchas116. All rights reserved.
+//
+
+import UIKit
+
+class NewsTabViewController: UIViewController {
+
+    @IBOutlet var reposView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
