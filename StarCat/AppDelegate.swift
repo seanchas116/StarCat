87c7657221d913fe98af291666baf56d6f58ba11
Ryohei Ikegami <iofg2100@gmail.com>
2015-12-30 17:31:13 +0900

    Change navigation bar font

--- StarCat/AppDelegate.swift
+++ StarCat/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         Authentication.accessToken = Constants.githubAccessToken
+        UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: UIFont(name: "AvenirNext-Medium", size: 18)!]
         return true
     }
     
