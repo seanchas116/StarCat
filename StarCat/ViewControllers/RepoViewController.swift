ad60ae5dfd9edd4579e1b5b9987d6b0789a09060
Ryohei Ikegami <iofg2100@gmail.com>
2016-01-03 00:45:34 +0900

    Add RepoViewController

---
+++ StarCat/ViewControllers/RepoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RepoViewController.swift
+//  StarCat
+//
+//  Created by Ryohei Ikegami on 2016/01/03.
+//  Copyright © 2016年 seanchas116. All rights reserved.
+//
+
+import UIKit
+
+class RepoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
