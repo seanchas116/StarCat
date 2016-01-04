9439faba83eebc4ed1c438122a2f92973e73683a
Ryohei Ikegami <iofg2100@gmail.com>
2016-01-04 20:42:30 +0900

    Configure left/right border of button

--- StarCat/Views/RoundButton.swift
+++ StarCat/Views/RoundButton.swift
@@ -25,6 +25,20 @@ class RoundButton: UIButton {
         }
     }
     
+    @IBInspectable
+    var hasLeftBorder: Bool = true {
+        didSet {
+            updateBorderShape()
+        }
+    }
+    
+    @IBInspectable
+    var hasRightBorder: Bool = true {
+        didSet {
+            updateBorderShape()
+        }
+    }
+    
     override var highlighted: Bool {
         didSet {
             updateBorderColor()
@@ -63,11 +77,46 @@ class RoundButton: UIButton {
     
     private func updateBorderShape() {
         let roundingCorners: UIRectCorner = [.TopRight, .TopLeft, .BottomRight, .BottomLeft]
-        borderLayer.path = UIBezierPath(
-            roundedRect: bounds.insetBy(dx: borderWidth * 0.5, dy: borderWidth * 0.5),
-            byRoundingCorners: roundingCorners,
-            cornerRadii: CGSizeMake(cornerRadius, cornerRadius)
-        ).CGPath
+        let halfBorder = borderWidth * 0.5
+        let path = UIBezierPath()
+        let width = bounds.width
+        let height = bounds.height
+        let pi = CGFloat(M_PI)
+        let cornerRadius = min(self.cornerRadius, width * 0.5)
+        
+        func addRightBorder() {
+            path.addLineToPoint(CGPoint(x: width - cornerRadius, y: halfBorder))
+            path.addArcWithCenter(CGPoint(x: width - cornerRadius, y: cornerRadius), radius: cornerRadius - halfBorder, startAngle: pi*1.5, endAngle: 0, clockwise: true)
+            path.addLineToPoint(CGPoint(x: width - halfBorder, y: height - cornerRadius))
+            path.addArcWithCenter(CGPoint(x: width - cornerRadius, y: height - cornerRadius), radius: cornerRadius - halfBorder, startAngle: 0, endAngle: pi*0.5, clockwise: true)
+        }
+        func addLeftBorder() {
+            path.addLineToPoint(CGPoint(x: cornerRadius, y: height - halfBorder))
+            path.addArcWithCenter(CGPoint(x: cornerRadius, y: height - cornerRadius), radius: cornerRadius - halfBorder, startAngle: pi*0.5, endAngle: pi, clockwise: true)
+            path.addLineToPoint(CGPoint(x: halfBorder, y: cornerRadius))
+            path.addArcWithCenter(CGPoint(x: cornerRadius, y: cornerRadius), radius: cornerRadius - halfBorder, startAngle: pi, endAngle: pi*1.5, clockwise: true)
+        }
+        
+        switch (hasLeftBorder, hasRightBorder) {
+        case (false, false):
+            path.moveToPoint(CGPoint(x: 0, y: halfBorder))
+            path.addLineToPoint(CGPoint(x: width, y: halfBorder))
+            path.moveToPoint(CGPoint(x: width, y: height - halfBorder))
+            path.addLineToPoint(CGPoint(x: 0, y: height - halfBorder))
+        case (false, true):
+            path.moveToPoint(CGPoint(x: 0, y: halfBorder))
+            addRightBorder()
+            path.addLineToPoint(CGPoint(x: 0, y: height - halfBorder))
+        case (true, false):
+            path.moveToPoint(CGPoint(x: width, y: height - halfBorder))
+            addLeftBorder()
+            path.addLineToPoint(CGPoint(x: width, y: halfBorder))
+        case (true, true):
+            path.moveToPoint(CGPoint(x: cornerRadius, y: halfBorder))
+            addRightBorder()
+            addLeftBorder()
+        }
+        borderLayer.path = path.CGPath
         borderLayer.lineWidth = borderWidth
     }
 }
