be2053efe16b245effab4e6fb768bd117669b3aa
Ryohei Ikegami <iofg2100@gmail.com>
2016-01-17 12:38:07 +0900

    Add search controller

--- StarCat/Sources/ViewControllers/SearchViewController.swift
+++ StarCat/Sources/ViewControllers/SearchViewController.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
-class SearchViewController: UITableViewController {
+class SearchViewController: UITableViewController, UISearchResultsUpdating {
+    
+    var searchController: UISearchController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.sizeToFit()
+        tableView.tableHeaderView = searchController.searchBar
+        
+        searchController.dimsBackgroundDuringPresentation = false
+        
+        definesPresentationContext = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        // TODO
     }
 }
