d7bb71a9825cefcea2ff503ff87ec72bd0e0325f
Ryohei Ikegami <iofg2100@gmail.com>
2016-01-27 00:54:03 +0900

    Remove parseLinkHeader

--- StarCatTests/utilSpec.swift
+++ StarCatTests/utilSpec.swift
@@ -11,33 +11,16 @@ import Quick
 import Nimble
 @testable import StarCat
 
-/*
 class UtilSpec: QuickSpec {
     override func spec() {
         
         describe("Util") {
-            describe("parseLinkHeader") {
-                it("parses HTTP link header"){
-                    let linkHeader =
-                    "<https://api.github.com/user/1025246/repos?page=3&per_page=100>; rel=\"next\", " +
-                        "<https://api.github.com/user/1025246/repos?page=1&per_page=100>; rel=\"prev\"; pet=\"cat\", " +
-                    "<https://api.github.com/user/1025246/repos?page=5&per_page=100>; rel=\"last\""
-                    
-                    let result = parseLinkHeader(linkHeader)
-                    expect(result).to(equal([
-                        "next": "https://api.github.com/user/1025246/repos?page=3&per_page=100",
-                        "prev": "https://api.github.com/user/1025246/repos?page=1&per_page=100",
-                        "last": "https://api.github.com/user/1025246/repos?page=5&per_page=100",
-                        ]))
-                }
-            }
             
             describe("String+SplitRegEx") {
                 it("split string by regex") {
-                    expect("foo🍣, bar,    baz".splitRegEx(",\\s+")).to(equal(["foo🍣", "bar", "baz"]))
+                    expect("foo🍣, bar,    baz".splitRegex(",\\s+")).to(equal(["foo🍣", "bar", "baz"]))
                 }
             }
         }
     }
 }
-*/
