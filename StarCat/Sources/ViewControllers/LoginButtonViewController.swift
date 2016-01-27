1efa9cc8051ede2f96e856ab2b7b13f0df77dbbc
Ryohei Ikegami <iofg2100@gmail.com>
2016-01-28 01:34:52 +0900

    Add LoginButtonView

---
+++ StarCat/Sources/ViewControllers/LoginButtonViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginButtonViewController.swift
+//  StarCat
+//
+//  Created by Ryohei Ikegami on 2016/01/28.
+//  Copyright © 2016年 seanchas116. All rights reserved.
+//
+
+import UIKit
+
+class LoginButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
