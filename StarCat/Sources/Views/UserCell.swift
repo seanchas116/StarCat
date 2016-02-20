19e6f8409f29ed28d4f112ed3dbf1525318a690d
Ryohei Ikegami <iofg2100@gmail.com>
2016-02-20 14:09:59 +0900

    Add user cell

---
+++ StarCat/Sources/Views/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  StarCat
+//
+//  Created by Ryohei Ikegami on 2016/02/20.
+//  Copyright © 2016年 seanchas116. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
