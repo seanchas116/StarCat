d59c60a7a22c3ef23935e87ca8e9f33530bd3d70
Ryohei Ikegami <iofg2100@gmail.com>
2016-03-06 23:25:43 +0900

    Add FileViewController

---
+++ StarCat/Sources/ViewControllers/FileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FileViewController.swift
+//  StarCat
+//
+//  Created by Ryohei Ikegami on 2016/03/06.
+//  Copyright © 2016年 seanchas116. All rights reserved.
+//
+
+import UIKit
+
+class FileViewController: UIViewController {
+
+}
