f8fc6b51d1f4035b3a0358d218826e938a6cdc3f
Ryohei Ikegami <iofg2100@gmail.com>
2016-03-12 11:58:55 +0900

    Improve highlight

--- StarCat/Sources/Services/Highlighter.swift
+++ StarCat/Sources/Services/Highlighter.swift
@@ -13,20 +13,25 @@ import PromiseKit
 struct Highlighter {
     private let highlightJS = getBundleFile("highlight.pack", ofType: "js")
     private let jsContext = JSContext()
-    private let highlightAuto: JSValue
+    private let hasLanguage: JSValue
+    private let highlight: JSValue
     private let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
     
     init() {
         let global = jsContext.globalObject
         global.setValue(global, forProperty: "window")
         jsContext.evaluateScript(highlightJS)
-        let hljs = global.valueForProperty("hljs")
-        highlightAuto = hljs.valueForProperty("highlightAuto")
+        hasLanguage = jsContext.evaluateScript("(function(lang) { return !!hljs.getLanguage(lang); })")
+        highlight = jsContext.evaluateScript("hljs.highlight")
     }
     
     func highlight(value: String, name: String) -> String {
-        let result = highlightAuto.callWithArguments([value])
-        return result.valueForProperty("value").toString()
+        if let ext = name.componentsSeparatedByString(".").last {
+            if hasLanguage.callWithArguments([ext]).toBool() {
+                return highlight.callWithArguments([ext, value]).valueForProperty("value").toString()
+            }
+        }
+        return value
     }
 }
 
